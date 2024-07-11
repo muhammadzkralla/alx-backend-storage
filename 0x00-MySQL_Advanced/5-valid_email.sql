b794e87813a967f2a29f58148bff7c4e1a29a3d3
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-12 00:34:13 +0300

    If Statements with Overrding the Delimiter

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Resets valid_email if the email changed
+
+DELIMITER //
+CREATE TRIGGER reset
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+	SET NEW.valid_email = 0;
+END IF;
+END; //
