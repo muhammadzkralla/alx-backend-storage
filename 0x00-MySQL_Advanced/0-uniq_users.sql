eb54c321a250c1316da36f20e1cc47cc8576cd53
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-11 22:04:34 +0300

    Unique Users Table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create users table if not exists
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
